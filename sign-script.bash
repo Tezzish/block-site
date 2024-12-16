01fd5ea57b7bee84ee5de66cd9b203689856e5b5
Tezzish <ishanpahwa14@gmail.com>
2024-12-16 03:35:42 +0100

    script: adds the auto-sign feature for future pipeline stuff

--- sign-script.bash
+++ sign-script.bash
@@ -1,3 +1,4 @@
-cp -r src build
+cp -r src test_auto_sign
+jq --argjson inc 1 '.version |= (split(".") | .[-1] = ((.[-1] | tonumber) + $inc | tostring) | join("."))' src/manifest.json > tmp.$$.json && mv tmp.$$.json src/manifest.json
 cd build
-web-ext sign --channel=unlisted --api-key=user:18580622:396 --api-secret=637fd542a13851a88407288b2268a507f87a47df22c6ff310e430e0a8d157d2d
+# web-ext sign --channel=unlisted --api-key=user:18580622:843 --api-secret=713db2576b93754f7da7f74bc044d5e8ac75fc745744b7aee0320af3bca3053e
